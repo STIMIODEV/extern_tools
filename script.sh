226eee1472efbf6ca852845f33a41db3f1496f2c
Ismael Fillonneau <ismael.fillonneau@stimio.fr>
2019-02-04 16:43:44 +0100

    add output folder

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 killall JLinkRTTClient
 killall JLinkExe
 
+mkdir -p "Donnees"
+
 heure=$(date +%Y_%m_%d_%Hh%Mmin)
 
 if [ $# == "1" ]; then
@@ -10,6 +12,6 @@ fi
 
 JLinkExe -device nrf52 -if swd -speed 4000 -autoconnect 1 &
 sleep 1
-JLinkRTTClient |ts '%H:%M:%S;' |cat > data_$name$heure.txt &
+JLinkRTTClient |ts '%H:%M:%S;' |cat > Donnees/data_$name$heure.txt &
 
-python3 live_accelerometer.py -input data_$name$heure.txt
+python3 live_accelerometer.py -input Donnees/data_$name$heure.txt
